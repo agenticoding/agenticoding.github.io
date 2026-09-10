695d5831771ebae95b4f5366d6fb55d7d07d1957
Ofri Wolfus <ofri.wolfus@gmail.com>
2026-09-10 16:29:27 +0300

    chore: remove obsolete idle emoji visuals

--- website/src/components/VisualElements/RobotEmoji.tsx
+++
@@ -1,6 +0,0 @@
-import InlineEmojiImage from './InlineEmojiImage';
-import { EMOJI } from './emojiAssets';
-
-export default function RobotEmoji({ size = 32 }: { size?: number }) {
-  return <InlineEmojiImage asset={EMOJI.agent} size={size} />;
-}
